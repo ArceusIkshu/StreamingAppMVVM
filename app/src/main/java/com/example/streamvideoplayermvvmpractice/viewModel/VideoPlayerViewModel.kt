00423112eaa19066d260dc8e20f06ea63e872d52
ikshu <ikshu.spatni@gmail.com>
2025-12-13 14:23:41 +0530

    Added ViewModel

--- app/src/main/java/com/example/streamvideoplayermvvmpractice/viewModel/VideoPlayerViewModel.kt
+++ app/src/main/java/com/example/streamvideoplayermvvmpractice/viewModel/VideoPlayerViewModel.kt
@@ -1,7 +1,40 @@
 package com.example.streamvideoplayermvvmpractice.viewModel
 
+import android.content.Context
+import android.net.Uri
 import androidx.lifecycle.ViewModel
+import androidx.media3.common.MediaItem
+import androidx.media3.exoplayer.ExoPlayer
+import com.example.streamvideoplayermvvmpractice.data.VideoData
 
 class VideoPlayerViewModel: ViewModel() {
+    private var exoPlayer: ExoPlayer? = null
+    var index: Int = 0
+    var videoList: List<VideoData> = listOf()
+
+    fun initializePlayer(context: Context){
+        exoPlayer = ExoPlayer.Builder(context).build()
+
+    }
+
+    fun releasePlayer(){
+        exoPlayer?.playWhenReady = false
+        exoPlayer?.release()
+        exoPlayer = null
+    }
+
+    fun playVideo(){
+        exoPlayer?.let{player->
+            player.apply{
+                stop()
+                clearMediaItems()
+                setMediaItem(MediaItem.fromUri(Uri.parse(videoList[index].videoUrl)))
+                playWhenReady = true
+                prepare()
+                play()
+            }
+
+        }
+    }
 
 }
